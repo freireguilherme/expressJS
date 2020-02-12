11f786695dcd18f54571efeb01f158e149d9a32e
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-02-12 18:43:26 -0300

    Excercício 04

--- program04.js
+++ program04.js
@@ -2,10 +2,10 @@ const express = require ('express');
 const bodyParser  = require ('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false})); //extended setado em falso para querystring
+app.use(bodyParser.urlencoded({extended: false}));       //extended setado em falso para querystring
 
-app.post('/form', (req, res) => {
-  res.send(req.body.str.split('').reverse().join(''))
+app.post('/form', (req, res) => {                       // metodo POST
+  res.send(req.body.str.split('').reverse().join(''))   //enviando a string invertida
 });
 
 app.listen(process.argv[2]);
